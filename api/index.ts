03800bf6bda09fab2b250bc1f0ad3954bc482a40
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2025-08-29 18:54:37 +0530

    Update index.ts SDFEDD

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
+// api/index.ts
 import serverless from "serverless-http";
 import app from "../src/index"; // path to your Express app
 
-export default serverless(app);
+export default serverless(app); // ✅ Vercel now gets a function
